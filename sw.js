03528ce160055c715048e8338ce4b4b52ae6c652
spsoohojeon-hash <sp.soohojeon@gmail.com>
2026-05-21 21:06:04 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME="randomchess-v1";
-const FILES_TO_CACHE=["/","/index.html","/style.css","/app.js","/cards.js","/manifest.json"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)))});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
