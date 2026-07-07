4ca1273571fc08aa77a061a7c20f69b6c7bbfb55
spsoohojeon-hash <sp.soohojeon@gmail.com>
2026-07-07 23:38:28 +0900

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCLHcT1IPTW_60G2eR1gWx5Ft78gR0_UAc",
-  authDomain: "randomechess-ab59f.firebaseapp.com",
-  projectId: "randomechess-ab59f",
-  storageBucket: "randomechess-ab59f.firebasestorage.app",
-  messagingSenderId: "140048881457",
-  appId: "1:140048881457:web:4526303c95476bfed98210",
-  measurementId: "G-8HVR0F6JEN"
-};
